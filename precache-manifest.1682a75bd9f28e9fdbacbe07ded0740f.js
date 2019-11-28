4c83101518e7afbb4ebfb36ecd57772ba6873519
Wesley Hatley <whatley@lenovo.com>
2019-11-27 21:24:36 -0500

    Updates

---
+++ precache-manifest.1682a75bd9f28e9fdbacbe07ded0740f.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec40e17bc33000c2a08c30566d8fd69",
+    "url": "/Black/index.html"
+  },
+  {
+    "revision": "bfce7fab3c327863bdd4",
+    "url": "/Black/static/css/2.9e63dcda.chunk.css"
+  },
+  {
+    "revision": "8cab0441a3069332e7ae",
+    "url": "/Black/static/css/main.26421e44.chunk.css"
+  },
+  {
+    "revision": "bfce7fab3c327863bdd4",
+    "url": "/Black/static/js/2.a89476fa.chunk.js"
+  },
+  {
+    "revision": "8cab0441a3069332e7ae",
+    "url": "/Black/static/js/main.a1ccdcd2.chunk.js"
+  },
+  {
+    "revision": "aa248cfb9dfd0157877d",
+    "url": "/Black/static/js/runtime~main.1116940a.js"
+  },
+  {
+    "revision": "17d118fe569961fe68dd00b6f57020dd",
+    "url": "/Black/static/media/bg1.17d118fe.jpg"
+  },
+  {
+    "revision": "3ef4d8894a068a6bd1f7549004cdefe6",
+    "url": "/Black/static/media/bg3.3ef4d889.jpg"
+  },
+  {
+    "revision": "48578653df646899392e3f31dd89fee2",
+    "url": "/Black/static/media/card-danger.48578653.png"
+  },
+  {
+    "revision": "3f053d3383eb1a890f131891702a65d8",
+    "url": "/Black/static/media/card-primary.3f053d33.png"
+  },
+  {
+    "revision": "e28474765d60fc66b2ede9c56d6b1792",
+    "url": "/Black/static/media/card-success.e2847476.png"
+  },
+  {
+    "revision": "24e54b66e5d6c4ed0e2bd11c8517f1d0",
+    "url": "/Black/static/media/card-warning.24e54b66.png"
+  },
+  {
+    "revision": "00401056d03c880bedd8dde300c11f0d",
+    "url": "/Black/static/media/emilyz.00401056.jpg"
+  },
+  {
+    "revision": "cabd1084133f5e7da7fce3c713a126cd",
+    "url": "/Black/static/media/gucci.cabd1084.png"
+  },
+  {
+    "revision": "c910111ff6eb2721d4aec15e8f10f0bd",
+    "url": "/Black/static/media/image_placeholder.c910111f.jpg"
+  },
+  {
+    "revision": "1fa38920cfbb4a4db04f212589ec5908",
+    "url": "/Black/static/media/jacket.1fa38920.png"
+  },
+  {
+    "revision": "7f18d6eb367b6355b8bb8487d8e9046e",
+    "url": "/Black/static/media/james.7f18d6eb.jpg"
+  },
+  {
+    "revision": "ffa18048dd5cf90ab1dd185ed47c7148",
+    "url": "/Black/static/media/jana.ffa18048.jpg"
+  },
+  {
+    "revision": "b2c3ec49c8fee950d6b99efd6389422f",
+    "url": "/Black/static/media/lora.b2c3ec49.jpg"
+  },
+  {
+    "revision": "8d9bbf30be68a521ae3192c52006d82a",
+    "url": "/Black/static/media/mike.8d9bbf30.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/Black/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/Black/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/Black/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/Black/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "9361df0cb00610df776ac7ee73cfa496",
+    "url": "/Black/static/media/react-logo.9361df0c.png"
+  },
+  {
+    "revision": "aace46001832512a7b61c28bae6b2956",
+    "url": "/Black/static/media/robi.aace4600.jpg"
+  },
+  {
+    "revision": "46eeebf1179f5bbd735c88e8981c88b5",
+    "url": "/Black/static/media/t-shirt.46eeebf1.png"
+  },
+  {
+    "revision": "ae5396e958ae44e086edb371f4b890aa",
+    "url": "/Black/static/media/tania.ae5396e9.jpg"
+  }
+]);
